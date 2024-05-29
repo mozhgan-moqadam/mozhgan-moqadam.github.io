4d40b6f9866e77fbf13640b5943605b72643a31b
Mozhgan Moqadam <mozhganmogadam@gmial.com>
2024-05-29 11:42:04 +0200

    imprt components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProfilComponent } from './profil/profil.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillComponent } from './skill/skill.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,ProfilComponent,EducationComponent,ExperienceComponent,SkillComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
