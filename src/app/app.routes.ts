5abfec2ac0ccd15a69afea239399ee246f3f13a5
Mozhgan Moqadam <mozhganmogadam@gmial.com>
2024-06-15 13:38:52 +0200

    header clicable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,22 +4,22 @@ import { ProfileComponent } from './profile/profile.component';
 import { HeaderComponent } from './header/header.component';
 import { SkillComponent } from './skill/skill.component';
 import { ExperienceComponent } from './experience/experience.component';
-import { EducationComponent } from './education/education.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 
 export const routes: Routes = [
   { path: '', component: ProfileComponent },
-  { path: 'about', component: HeaderComponent },
-  { path: 'skills', component: SkillComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'skill', component: SkillComponent },
   { path: 'experience', component: ExperienceComponent },
-  { path: 'education', component: EducationComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'about-me', component: AboutMeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', redirectTo: 'header' } 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
